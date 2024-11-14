85646d2a89ef693576888c348c8edd91b7fab0ea
Stevani15 <stevaninadeak.15@gmail.com>
2024-11-14 15:57:46 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import './HomePage.js';
-import './HomePage.css';
+import HomePage from './HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React3
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
