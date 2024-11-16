3cc31794a380d3e1895e60a99407dafc9e66ea95
EpelinaPangaribuan <evelinapangaribuan153@gmail.com>
2024-11-16 21:51:52 +0700

    Epelina
    memperbaiki error di medan magnetik

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import MedanMagnetik from './components/MedanMagnetik';
+import HomePage from './pages/HomePage'; // Impor file HomePage
+import MedanMagnetik from './components/MedanMagnetik'; // Impor halaman Medan Magnetik
+
 function App() {
   return (
     <Router>
       <div className="App">
         {/* Navigation Bar */}
-        <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>      
-          <Link to="/MedanMagnetik" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>MedanMagnetik</Link>
-          </nav>
-        <Routes>  
+        <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
+          <Link 
+            to="/" 
+            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
+          >
+            Home
+          </Link>
+          <Link 
+            to="/MedanMagnetik" 
+            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
+          >
+            Medan Magnetik
+          </Link>
+        </nav>
+
+        {/* Routes untuk halaman */}
+        <Routes>
+          {/* Route untuk Homepage */}
+          <Route path="/" element={<HomePage />} />
+          {/* Route untuk Halaman Medan Magnetik */}
           <Route path="/MedanMagnetik" element={<MedanMagnetik />} />
         </Routes>
       </div>
