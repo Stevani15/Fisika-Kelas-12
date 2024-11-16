a97a9789e8a166c389a9c59800aaa15ec29b4001
EpelinaPangaribuan <evelinapangaribuan153@gmail.com>
2024-11-16 16:43:58 +0700

    Epelina
    telah ditambahkan materi medan magnet namun belum terhubung dengan HomePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+// src/App.js
 import React from 'react';
-import './App.css';
-import HomePage from './HomePage';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import MedanMagnetik from './components/MedanMagnetik';
 function App() {
-    return (
-        <div className="App">
-            <HomePage />
-        </div>
-    );
+  return (
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>      
+          <Link to="/MedanMagnetik" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>MedanMagnetik</Link>
+          </nav>
+        <Routes>  
+          <Route path="/MedanMagnetik" element={<MedanMagnetik />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
