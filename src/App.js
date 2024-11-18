993162cd216c0f5156f2f315d2667849704af823
EpelinaPangaribuan <evelinapangaribuan153@gmail.com>
2024-11-18 08:01:25 +0700

    Memperbaiki halaman homepage yang error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage'; // Impor file HomePage
+import HomePage from './components/HomePage'; // Impor file HomePage
+import './components/HomePage.css';
 import MedanMagnetik from './components/MedanMagnetik'; // Impor halaman Medan Magnetik
 
 function App() {
