d69e9407e08396f0b0e359b72643471435f4eeb8
jamotsigiro41424017 <jamotsigiro35@gmail.com>
2024-11-20 11:01:44 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import TeknologiDigital from './components/TeknologiDigital';
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
       <div className="App">
         <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
           <Link 
@@ -73,10 +72,8 @@ function App() {
             Gelombang Elektromagnetik
           </Link>
         </nav>
-
+        </div>
         {/* Routes untuk halaman */}
-=======
->>>>>>> 8b696ad901c8e75ba0a3b3866ec7f1105d114470
         <Routes>
           {/* Route untuk Homepage */}
           <Route path="/" element={<HomePage />} />
