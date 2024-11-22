479b3a6fa20feb31a33e0de126e0d2ad750ba1b7
Lastrihtgl <hutagalunglastri3@gmail.com>
2024-11-22 09:23:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, Form } from 'react-router-dom';
-import HomePage from './components/HomePage'; // Impor file HomePage
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from './components/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/HomePage.css';
-import MedanMagnetik from './components/MedanMagnetik'; // Impor halaman Medan Magnetik
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import MedanMagnetik from './components/MedanMagnetik'; 
 import SumberEnergi from './components/SumberEnergi';
 import FisikaModern from './components/FisikaModern';
 import GelombangElektromagnetik from './components/GelombangElektromagnetik';
@@ -16,75 +17,98 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav style={{ padding: '10px', background: '#333', color: '#fff' }}>
-          <Link to="/" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Home
-          </Link>
-          <Link to="/MedanMagnetik" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}  >
-            Medan Magnetik
-          </Link>
-          <Link 
-            to="/SumberEnergi" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-          
-            Sumber Energi
-          </Link>
-          <Link 
-            to="/TeknologiDigital" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Teknologi Digital
-          </Link>
-          <Link 
-            to="/FisikaModern" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Fisika Modern
-          </Link>
-          <Link 
-            to="/RangkaianArusBolakBalik" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Rangkaian Arus Bolak Balik
-          </Link>
-          <Link 
-            to="/InduksiElektromagnetik" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Induksi Elektromagnetik
-          </Link>
-          <Link 
-            to="/RangkaianArusSearah" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Rangkaian Arus Searah
-          </Link>
-          <Link 
-            to="/GelombangElektromagnetik" 
-            style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}
-          >
-            Gelombang Elektromagnetik
-          </Link>
+        {/* Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              Physics App
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                {/* Home Link */}
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+
+                {/* Dropdown Menu */}
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle"
+                    to="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Materi Fisika
+                  </Link>
+                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <Link className="dropdown-item" to="/MedanMagnetik">
+                        Medan Magnetik
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/SumberEnergi">
+                        Sumber Energi
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/TeknologiDigital">
+                        Teknologi Digital
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/FisikaModern">
+                        Fisika Modern
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/RangkaianArusBolakBalik">
+                        Rangkaian Arus Bolak Balik
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/InduksiElektromagnetik">
+                        Induksi Elektromagnetik
+                      </Link>
+                    </li>
+                    <li>
+                      
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
-        </div>
-        {/* Routes untuk halaman */}
+
+        {/* Routes */}
         <Routes>
-          {/* Route untuk Homepage */}
           <Route path="/" element={<HomePage />} />
-          {/* Route untuk Halaman Medan Magnetik */}
           <Route path="/MedanMagnetik" element={<MedanMagnetik />} />
           <Route path="/SumberEnergi" element={<SumberEnergi />} />
           <Route path="/TeknologiDigital" element={<TeknologiDigital />} />
           <Route path="/FisikaModern" element={<FisikaModern />} />
           <Route path="/RangkaianArusBolakBalik" element={<RangkaianArusBolakBalik />} />
           <Route path="/InduksiElektromagnetik" element={<InduksiELektromagnetik />} />
-          <Route path="/GelombangElektromagnetik" element={<GelombangElektromagnetik />} />
           <Route path="/RangkaianArusSearah" element={<RangkaianArusSearah />} />
+          <Route path="/GelombangElektromagnetik" element={<GelombangElektromagnetik />} />
         </Routes>
+      </div>
     </Router>
   );
 }
