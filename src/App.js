90c73a94f4c51bac68f65c6f7d28bde7eecab31e
Lastrihtgl <hutagalunglastri3@gmail.com>
2024-11-29 15:06:00 +0800

    Memperbaiki HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage'; // Pastikan path ini benar
-import Bab1 from './components/Bab1'; // Pastikan path ini benar
+import HomePage from './components/HomePage'; 
+import Bab1 from './components/Bab1'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
@@ -9,11 +9,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Halaman Utama */}
         <Route path="/" element={<HomePage />} />
-
-        {/* Halaman Bab 1 */}
         <Route path="/bab1" element={<Bab1 />} />
+        <Route path="/bab2" element={<Bab2 />} />
+
       </Routes>
     </Router>
   );
