ca3878d66d7aef454ce5fee8012a4104f2a6ef0f
EpelinaPangaribuan <evelinapangaribuan153@gmail.com>
2024-12-04 07:43:00 +0700

    menambah materi

--- src/components/Bab2.js
+++ src/components/Bab2.js
@@ -1,46 +1,59 @@
 import React from "react";
 import '../styles/Bab.css';
 import { Link } from "react-router-dom";
-import gbdsimages from '../images/induksi.jpg';
-import gbrsimages from '../images/faraday.jpg';
-import stkimages from '../images/pengaruh.jpg';
+import medanMagnetikImage from '../images/medan_magnetik.png'; // Gambar medan magnetik
 
-function Bab1() {
-    const jurusanData = [
+function Bab2() {
+    const materiData = [
       {
-        nama: "Pengertian Induksi Elektromagnetik",
+        nama: "Medan Magnet",
         deskripsi:
-          "Materi tentang geometri bidang datar pada umumnya menjelaskan tentang sebuah jarak, baik jarak antara titik dengan garis maupun antara dua titik",
-        gambar: gbdsimages,
-        path: "/images/induksi.jpg",
-      },
-      {
-        nama: "Hukum Faraday",
-        deskripsi:
-          "bangun ruang adalah sebutan untuk bangun-bangun yang berbentuk tiga dimensi atau bangun yang memiliki ruang yang di batasi dengan sisi-sisi nya.",
-        gambar: gbrsimages,
-        path: "/images/faraday.jpg",
-      },
-      {
-        nama: "Statistika",
-        deskripsi:
-          "ilmu yang memfokuskan pada cara merancang, menghimpun, menganalisis, menafsirkan, dan menyajikan data. ",
-        gambar: stkimages,
-        path: "/components/pengaruh.jpg",
+          "Medan magnet adalah wilayah di sekitar magnet yang memengaruhi benda dengan sifat-sifat magnetik. Medan ini terbentuk ketika muatan bergerak dan memberikan gaya pada muatan bergerak lainnya.",
+        gambar: medanMagnetikImage,
+        rumus: [
+          "B = μ₀ * n * I", // Rumus medan magnet pada solenoida
+          "B = F / (q * v * sin(θ))", // Rumus medan magnet Tesla
+          "B = (μ₀ * N * I) / (2π * r)", // Rumus medan magnet pada toroida
+        ],
+        sifat: [
+          "Medan magnet dapat menarik benda-benda yang memiliki sifat-sifat magnetik, seperti logam.",
+          "Setiap magnet memiliki dua kutub, yaitu kutub utara dan kutub selatan, yang selalu berpasangan.",
+          "Garis medan magnet selalu mengarah dari kutub utara menuju kutub selatan.",
+          "Medan magnet membentuk pola lingkaran di sekitar magnet, yang semakin kuat mendekati kutub.",
+          "Medan magnet menjadi lebih lemah seiring jaraknya dari sumber magnet.",
+          "Medan magnet dapat menginduksi arus listrik dalam benda konduktor yang bergerak melintasi medan tersebut."
+        ],
+        path: "/images/medan_magnetik.jpg",
       },
     ];
 
     return (
         <div className="jurusan-container">
-            <h1>Materi</h1>
+            <h1>Materi Medan Magnetik</h1>
             <div className="jurusan-cards">
-            {jurusanData.map((jurusan, index) => (
+            {materiData.map((materi, index) => (
                 <div key={index} className="jurusan-card">
-                    <img src={jurusan.gambar} alt={jurusan.nama} className="jurusan-image" />
-                    <Link to={jurusan.path}>
-                        <h2>{jurusan.nama}</h2>
+                    <img src={materi.gambar} alt={materi.nama} className="jurusan-image" />
+                    <Link to={materi.path}>
+                        <h2>{materi.nama}</h2>
                     </Link>
-                    <p>{jurusan.deskripsi}</p>
+                    <p>{materi.deskripsi}</p>
+                    <div>
+                        <h3>Sifat-Sifat Medan Magnet:</h3>
+                        <ul>
+                            {materi.sifat.map((sifat, i) => (
+                                <li key={i}>{sifat}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Rumus-Rumus Medan Magnet:</h3>
+                        <ul>
+                            {materi.rumus.map((rumus, i) => (
+                                <li key={i}>{rumus}</li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             ))}
             </div>
@@ -48,4 +61,4 @@ function Bab1() {
     );
 }
 
-export default Bab1;
+export default Bab2;
